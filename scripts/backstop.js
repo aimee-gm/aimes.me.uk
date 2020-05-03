71914dad2a1ca6a0e82dee3a5f8105d4b9adf576
Aimee Gamble-Milner <12508200+aimee-gm@users.noreply.github.com>
2020-05-03 17:20:48 +0100

    test(screenshots): add screenshot testing (#57)
    * test(screenshots): add screenshot testing
    
    * ci: add screenshot_testing
    
    * ci: upload artifacts [skip netlify]
    
    * chore(deps): upgrade dependencies

---
+++ scripts/backstop.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const backstop = require("backstopjs");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "../dist")));
+
+const server = app.listen(5000, () => {
+  app.emit("ready", null);
+});
+
+app.on("ready", async () => {
+  try {
+    await backstop("test");
+    server.close(() => {
+      process.exit(0);
+    });
+  } catch (err) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+});
