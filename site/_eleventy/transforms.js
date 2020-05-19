964a5d7eba48a0278fdb88858d53c8e6358f44b4
Aimee Gamble-Milner <12508200+aimee-gm@users.noreply.github.com>
2020-05-19 08:25:02 +0100

    test(html): fix self closing tags (#91)

--- site/_eleventy/transforms.js
+++ site/_eleventy/transforms.js
@@ -8,11 +8,22 @@ module.exports = {
       });
     }
   },
-  thScope: (content, outputPath) => {
-    if (outputPath.endsWith(".html")) {
+  validHtml: (content, outputPath) => {
+    if (!outputPath.endsWith(".html") || !content) {
+      return content;
+    }
+
+    if (content.includes("<th>")) {
       // this is crude, but makes it accessible and pass HTML validation
       // this breaks down if we ever have the need for scope="row"
-      return content.replace(/<th>/g, '<th scope="col">');
+      content = content.replace(/<th>/g, '<th scope="col">');
     }
+
+    if (content.includes("<br>")) {
+      // eleventy syntax highlighter uses <br>
+      return content.replace(/<br>/g, "<br/>");
+    }
+
+    return content;
   },
 };
